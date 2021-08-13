473087b287704a70658f90bc9aef4740507d75a0
ItaiAxelrad <itaiaxelrad@gmail.com>
2021-08-12 21:44:22 -0600

    make def

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
-# Environmental Compliance Information Management System
+# Groundwater Protection Monitoring Report
 
-Leppert Associates provides this universal work to delineate resources required to adjoin a new Facility to the company’s Environmental Compliance Information Management System (ECIMS). The work plan consists of four (4) carefully designed tasks.
-
-1. Facility Compliance Monitoring Program Audit
-2. Data Loading
-3. Perform Facility regulatory permit optimization
-4. Define activities to be performed for recurring Compliance Monitoring Program Administration.
-
-Read more about the program in the [docs](docs/ECIMS.md).
+A tool used in anaylzing and visualizing monitoring data for reports. 
 
 ## Setup
 
--- main.py
+++ main.py
@@ -1,40 +1,44 @@
-from utils.population import Population
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+# Create plots using lists of locations and parameters
+def plot_figs(df, locs, params):
+    dfmi = df.set_index(['Location', 'Parameter', 'Time'],
+                        append=False).sort_index()
+    for loc in locs:
+        for param in params:
+            fig = plt.figure(figsize=(10, 7.5))
+            ax = fig.add_subplot(111)
+            for p in params[param]:
+                sub_df = dfmi.loc[(loc, p)].reset_index(level='Time')
+                sub_df.plot(x='Time', y='Value', kind='line',
+                            logy=True, grid=True, subplots=False, marker='o', label=p, title=f"{loc} {param}", ax=ax)
+            ax.set_ylabel(f"Concentration ({sub_df.Unit[0]})")
+            ax.legend(loc='center left', frameon=False,
+                      bbox_to_anchor=(1, 0.5))
+            plt.tight_layout()
+            fig.savefig(f"output/{loc}_{param}", orientation='landscape')
+            plt.clf()
+            plt.close('all')
+
 
 # Put in postgres db...
 # Facility wide 'raw' data
 df = pd.read_csv(r'data/deertrail.csv', parse_dates=['Time'], engine='python')
 
 # # Multi-Index dataframe
-# multi = df.set_index(['Location', 'Parameter', 'Time']).sort_index()
+multi = df.set_index(['Location', 'Parameter', 'Time']).sort_index()
 
-# # 'Subpopulations'
-# locs = df['Location'].unique()
-# analytes = df['Parameter'].unique()
 
 # Should be table in db that is brought in as multi-index
 locs = ['L3-42']  # df['Location'].unique()
-params1 = {"Metals": ['7439-89-6', '7439-92-1', '7439-96-5', '7439-97-6', '7440-02-0', '7440-22-4',
-                      '7440-23-5', '7440-38-2', '7440-43-9', '7440-47-3', '7440-66-6', '7740-39-3', '7782-49-2']}
-
+cas = {"Metals": ['7439-89-6', '7439-92-1', '7439-96-5', '7439-97-6', '7440-02-0', '7440-22-4',
+                  '7440-23-5', '7440-38-2', '7440-43-9', '7440-47-3', '7440-66-6', '7740-39-3', '7782-49-2']}
 params = {"Inorganic": ['ARSENIC (DISSOLVED)', 'Arsenic (TOTAL)', 'Chloride', 'Fluoride',
                         'Nitrogen, Nitrate-Nitrite', 'Organic Carbon, Total', 'Sulfate', 'Zinc (TOTAL)']}
 
 
-# Create plots
-for loc in locs:
-    for param in params:
-        fig = plt.figure(figsize=(10, 7.5))
-        ax = fig.add_subplot(111)
-        for p in params[param]:  # .isin(params)
-            sub_df = df[(df['Location'] == loc) &
-                        (df['Parameter'] == p)]
-            sub_df.plot(x='Time', y='Value', kind='line',
-                        logy=True, grid=True, subplots=False, marker='o', label=p, title=f"{loc} {param}", ax=ax)
-        ax.set_ylabel(f"Concentration ()")
-        ax.legend(loc='center left', frameon=False, bbox_to_anchor=(1, 0.5))
-        plt.tight_layout()
-        fig.savefig(f"output/{loc}_{param}", orientation='landscape')
-        plt.clf()
-        plt.close('all')
+# need to check if we get a hit for every analyte in the permit
+# should really just add a column for analyte group to db
+plot_figs(df, locs, params)
